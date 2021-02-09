9fecde3b7f2a985708af2de0bfdf1d37a77407b8
Sepehr Moghiseh <35396646+sepehrmoghiseh@users.noreply.github.com>
2021-02-09 13:31:13 +0330

    final ver uploaded

--- module_02/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/project/CE202-LC-Lab-Manual/Projects/module 2/GasDetectorSensor_isim_beh.exe" -prj "D:/project/CE202-LC-Lab-Manual/Projects/module 2/GasDetectorSensor_beh.prj" "work.GasDetectorSensor" "work.glbl" 
